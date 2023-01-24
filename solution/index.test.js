04fad23cd8a3793f20e74a2aa453de28025cbeb4
Andrew <70034920+HITAND@users.noreply.github.com>
2023-01-24 16:29:12 +0200

    Add files via upload

---
+++ solution/index.test.js
@@ -0,0 +1,18 @@
+﻿import {} from "./calculator";
+
+it("17 и в Африке 17", () => {
+  expect(17).toEqual(17);
+});
+
+it("17 и в Африке 17", () => {
+  expect(18).not.toEqual(17);
+});
+
+it("should get only even numbers from array", () => {
+  const getEvenNumbers = (numbers) => {
+    return numbers.filter((num) => num % 2 === 0);
+  };
+  const result = getEvenNumbers([1, 2, 4]);
+
+  expect(result).toEqual([2, 4]);
+});
