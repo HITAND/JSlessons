7f0efa27f3886cc2ece9c840cac59ef530b03a7b
Andrew <70034920+HITAND@users.noreply.github.com>
2023-01-25 18:12:04 +0200

    Add files via upload

---
+++ solution/getMinSquaredNumber.test.js
@@ -0,0 +1,22 @@
+﻿import { getMinSquaredNumber } from "./getMinSquaredNumber";
+
+it("should get array with numbers", () => {
+  const result = getMinSquaredNumber([]);
+  if (expect([].length).toEqual(0)) {
+    return null;
+  }
+});
+
+it("should get array with numbers", () => {
+  const result = getMinSquaredNumber("");
+  if (expect(typeof "").toEqual("string")) {
+    return null;
+  }
+});
+
+it("should get array with numbers", () => {
+  const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
+  if (expect(result).toEqual(4)) {
+    return result;
+  }
+});
