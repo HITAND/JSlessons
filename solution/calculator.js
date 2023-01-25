917adb6be700a63472b1f78a7a4bc3125836e3df
Andrew <70034920+HITAND@users.noreply.github.com>
2023-01-25 17:19:18 +0200

    Add files via upload

---
+++ solution/calculator.js
@@ -0,0 +1,34 @@
+﻿/* eslint-disable default-case */
+
+export function reverseArray(arr) {
+  // put your code here
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  const [...arr1] = arr;
+  return [...arr1].reverse();
+}
+
+export const withdraw = (clients, balances, client, amount) => {
+  // put your code here
+  if (clients[1] === client) {
+    return balances[1] - amount;
+  }
+  return -1;
+};
+
+export const getAdults = (obj) => {
+  // put your code here
+  let elem;
+  const obj1 = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (elem in obj) {
+    if (obj[elem] >= 18) {
+      obj1[elem] = obj[elem];
+    }
+  }
+  return obj1;
+};
+
+// examples
+// { 'John Doe': 19, Bob: 18 }
