2a6ff422545c06516be03a060b912358ed41caee
Andrew <70034920+HITAND@users.noreply.github.com>
2023-02-08 18:18:16 +0200

    Update main.js

--- solution/main.js
+++ solution/main.js
@@ -3,13 +3,13 @@
 		{name: 'Tom', email:'t@gmail.com', age: 17},
 		{name: 'Bob', email:'b@gmail.com', age: 18},
 	],
-	message:'Welcome to the party',
+	message:'Welcome to the party!',
 	getInvitations() {
 		return this.guests
 		.filter(({age}) => age >= 18)
 		.map(({name, email}) => ({
 			email,
-			text: `Dear ${name}! ${this.message}!`,
+			text: `Dear ${name}! ${this.message}`,
 		}))
 	}
 }
