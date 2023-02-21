84f5f19170ce5ee3856146e7ef2f0cbbd2bed470
Andrew <70034920+HITAND@users.noreply.github.com>
2023-02-21 14:59:15 +0200

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -2,12 +2,16 @@ export const getTitleElement = () => {
 	const titleElem = document.querySelector(".title");
 
 	console.dir(titleElem);
+	
+	return titleElem;
 }
 
 export const getInputElement = () => {
 	const inputElem = document.querySelector('input');
 
 	console.dir(inputElem);
+	
+	return inputElem;
 }
 
 getTitleElement()
