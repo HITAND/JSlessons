c94419ea23942dfcb7ef15680be92aba2f2dbd8c
Andrew <70034920+HITAND@users.noreply.github.com>
2023-02-21 15:23:29 +0200

    Add files via upload

---
+++ tests/index.js
@@ -0,0 +1,15 @@
+﻿export const getItemsList = () => {
+	const elementsList = document.querySelectorAll('.technology');
+
+	console.dir(elementsList);
+
+	return elementsList;
+}
+
+export const getItemsArray = () => {
+	const elementsArray = document.querySelectorAll('.tool');
+
+	Array.from(elementsArray);
+	console.dir(elementsArray);
+	return elementsArray;
+}
