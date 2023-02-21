4d30da8d0286427338c47f74d8e28f7495dabf32
Andrew <70034920+HITAND@users.noreply.github.com>
2023-02-21 14:57:08 +0200

    Add files via upload

---
+++ tests/index.js
@@ -0,0 +1,18 @@
+﻿const getTitleElement = () => {
+	const titleElem = document.querySelector(".title");
+
+	console.dir(titleElem);
+
+  return titleElem;
+}
+
+const getInputElement = () => {
+	const inputElem = document.querySelector('input');
+
+	console.dir(inputElem);
+
+  return inputElem;
+}
+
+getTitleElement()
+getInputElement()
