b2e388cc7b2d0737c563f86162ffc90580646a09
Andrew <70034920+HITAND@users.noreply.github.com>
2023-02-27 17:30:46 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -28,3 +28,14 @@ pElem.addEventListener('click', logGreenP);
 
 spanElem.addEventListener('click', logGreySpan, true);
 spanElem.addEventListener('click', logGreenSpan);
+
+attachHandlersBtn.addEventListener('click', () => {
+divElem.removeEventListener('click', logGreyDiv, true);
+divElem.removeEventListener('click', logGreenDiv);
+
+pElem.removeEventListener('click', logGreyP, true);
+pElem.removeEventListener('click', logGreenP);
+
+spanElem.removeEventListener('click', logGreySpan, true);
+spanElem.removeEventListener('click', logGreenSpan);
+})
