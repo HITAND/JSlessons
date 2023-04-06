1547025a1dec4744a3fdf97e55f6dc90fa562e08
Andrew <70034920+HITAND@users.noreply.github.com>
2023-04-06 17:45:55 +0300

    Add files via upload

---
+++ Babel/src/index.js
@@ -0,0 +1,13 @@
+﻿import { printProfile } from "./profile"
+
+const userData = {
+  name: "Tom",
+  age: 17,
+}
+
+const profile = {
+  ...userData,
+  company: "Gromcode",
+}
+
+printProfile(profile)
